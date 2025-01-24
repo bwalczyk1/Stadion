9c52b44365a6b0328a07d3142c37c32565b668eb
Bartosz Walczyk <bartosz.walczyk@fingoweb.com>
2025-01-24 13:27:15 +0100

    improved error handling

--- helpers.c
+++ helpers.c
@@ -29,7 +29,7 @@ key_t getFtokKey(int keyID) {
     key_t ftokKey = ftok(".", keyID);
 
     if (ftokKey == -1) {
-        printf("Blad ftok (%d)\n", getpid());
+        perror("Blad ftok ");
         exit(1);
     }
 
@@ -41,7 +41,7 @@ int initializeMessageQueue(int keyID) {
     int msgID = msgget(ftokKey, IPC_CREAT | 0666);
 
     if (msgID == -1) {
-        printf("Blad kolejki komunikatow (%d)\n", getpid()); 
+        perror("Blad kolejki komunikatow: "); 
         exit(1);
     }
 
@@ -53,7 +53,7 @@ int initializeSharedMemory(int keyID, int shmSize) {
     int shmID = shmget(ftokKey, shmSize, IPC_CREAT | 0666);
     
     if (shmID == -1) {
-        printf("Blad pamieci dzielonej (%d)\n", getpid());
+        perror("Blad pamieci dzielonej:");
         exit(1);
     }
 
@@ -62,28 +62,28 @@ int initializeSharedMemory(int keyID, int shmSize) {
 
 void sendMessage(int msgID, struct msg* message) {
     if (msgsnd(msgID, message, sizeof(message->mValue), 0) == -1) {
-        printf("Blad wyslania komunikatu (%d)\n", getpid());
+        perror("Blad wyslania komunikatu:");
         exit(1);
     }
 }
 
 void sendMessageWithCounter(int msgID, struct msgCounter* messageWithCounter) {
     if (msgsnd(msgID, messageWithCounter, sizeof(messageWithCounter->mValueWithCounter), 0) == -1) {
-        printf("Blad wyslania komunikatu (%d)\n", getpid());
+        perror("Blad wyslania komunikatu: ");
         exit(1);
     }
 }
 
 void receiveMessage(int msgID, struct msg* message, int msgType) {
     if (msgrcv(msgID, message, sizeof(message->mValue), msgType, 0) == -1) {
-        printf("Blad odbioru komunikatu (%d)\n", getpid());
+        perror("Blad odbioru komunikatu: ");
         exit(1);
     }
 }
 
 void receiveMessageWithCounter(int msgID, struct msgCounter* messageWithCounter, int msgType) {
     if (msgrcv(msgID, messageWithCounter, sizeof(messageWithCounter->mValueWithCounter), msgType, 0) == -1) {
-        printf("Blad odbioru komunikatu (%d)\n", getpid());
+        perror("Blad odbioru komunikatu: ");
         exit(1);
     }
 }
@@ -94,28 +94,28 @@ int allocateSem(int keyID, int number)
     int semID= semget(ftokKey, number, IPC_CREAT | 0666);
     
     if (semID== -1) {
-        perror("Blad semget (alokujSemafor): ");
+        perror("Blad semget: ");
         exit(1);
     }
 
     return semID;
 }
 
-int freeSem(int semID, int number)
+void freeSem(int semID, int number)
 {
-    return semctl(semID, number, IPC_RMID, NULL);
+    semctl(semID, number, IPC_RMID, NULL);
 }
 
 void initializeSem(int semID, int number, int val)
 {
     if ( semctl(semID, number, SETVAL, val) == -1 )
     {
-        perror("Blad semctl (inicjalizujSemafor): ");
+        perror("Blad semctl: ");
         exit(1);
     }
 }
 
-int waitSem(int semID, int number)
+void waitSem(int semID, int number)
 {
     int result;
     struct sembuf operacje[1];
@@ -125,11 +125,9 @@ int waitSem(int semID, int number)
     
     if ( semop(semID, operacje, 1) == -1 )
     {
-        perror("Blad semop (waitSemafor)");
-        return -1;
+        perror("Blad semop");
+        exit(1);
     }
-    
-    return 1;
 }
 
 void signalSem(int semID, int number)
@@ -137,10 +135,9 @@ void signalSem(int semID, int number)
     struct sembuf operacje[1];
     operacje[0].sem_num = number;
     operacje[0].sem_op = 1;
-    //operacje[0].sem_flg = SEM_UNDO;
 
     if (semop(semID, operacje, 1) == -1 )
-        perror("Blad semop (postSemafor): ");
+        perror("Blad semop: ");
 }
 
 int getSemValue(int semID, int number)
